3c58361104e19483d8f5b1c2e611409e01eabf88
unic <2445942195@qq.com>
2024-10-19 00:25:46 +0800

    feat: 添加 clap 依赖并实现命令行 curl 解析功能
    - 在 Cargo.toml 中添加 clap 依赖
    - 在 src/main.rs 中使用 clap 实现命令行接口
    - 添加 curl 命令解析功能
    - 实现将 curl 命令拆分为不同部分的功能

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,93 @@
+use clap::{Arg, Command};
+use curl::{curl_parsers::curl_cmd_parse, Curl};
+
+pub mod curl;
+mod test_util;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, clap::ValueEnum)]
+pub enum CurlCommand {
+    Method,
+    Header,
+    Data,
+    Flag,
+    Url,
+}
+
+// TODO: Build more funcs
 fn main() {
-    println!("Hello, world!");
+    let matches = Command::new("nomcurl")
+        .version("0.1.0")
+        .about("A CLI tool to parse and manipulate curl commands")
+        .subcommand_required(true)
+        .arg_required_else_help(true)
+        .subcommand(
+            Command::new("parse")
+                .about("Parses a curl command")
+                .arg(
+                    Arg::new("command")
+                        .help("The input curl command string")
+                        .required(true)
+                        .index(1),
+                )
+                .arg(
+                    Arg::new("part")
+                        .short('p')
+                        .long("part")
+                        .value_name("PART")
+                        .help("Specifies which part of the curl command to parse (method, header, data, flag, url)")
+                        .required(false)
+                        .value_parser(clap::value_parser!(CurlCommand)),
+                ),
+        )
+        .get_matches();
+
+    match matches.subcommand() {
+        Some(("parse", sub_matches)) => {
+            let command = sub_matches.get_one::<String>("command").unwrap();
+            let part = sub_matches.get_one::<CurlCommand>("part");
+
+            match curl_cmd_parse(command) {
+                Ok((_remaining, curls)) => {
+                    if let Some(part) = part {
+                        match part {
+                            CurlCommand::Method => {
+                                for curl in curls.iter().filter(|c| matches!(c, Curl::Method(_))) {
+                                    println!("{:?}", curl);
+                                }
+                            }
+                            CurlCommand::Header => {
+                                for curl in curls.iter().filter(|c| matches!(c, Curl::Header(_))) {
+                                    println!("{:?}", curl);
+                                }
+                            }
+                            CurlCommand::Data => {
+                                for curl in curls.iter().filter(|c| matches!(c, Curl::Data(_))) {
+                                    println!("{:?}", curl);
+                                }
+                            }
+                            CurlCommand::Flag => {
+                                for curl in curls.iter().filter(|c| matches!(c, Curl::Flag(_))) {
+                                    println!("{:?}", curl);
+                                }
+                            }
+                            CurlCommand::Url => {
+                                for curl in curls.iter().filter(|c| matches!(c, Curl::URL(_))) {
+                                    println!("{:?}", curl);
+                                }
+                            }
+                        }
+                    } else {
+                        for curl in curls {
+                            println!("{:?}", curl);
+                        }
+                    }
+                }
+                Err(e) => eprintln!("Error parsing curl command: {:?}", e),
+            }
+        }
+        _ => {
+            Command::new("nomcurl").print_help().unwrap();
+            println!();
+        }
+    }
 }
