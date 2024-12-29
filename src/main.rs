d19c0064a06a2e0edcf54a22c08fa426a0862b78
ohmycloud <ohmycloudy@gmail.com>
2024-12-29 22:43:24 +0800

    use derive macro of clap

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use clap::{Arg, Command};
+use clap::{Command, Parser, Subcommand};
 use curl::{curl_parsers::curl_cmd_parse, Curl};
 
 pub mod curl;
@@ -13,78 +13,70 @@ pub enum CurlCommand {
     Url,
 }
 
-// TODO: Build more funcs
+#[derive(Parser)]
+#[command(name = "nomcurl")]
+#[command(version = "0.1.0")]
+#[command(about = "A CLI tool to parse and manipulate curl commands")]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    #[command(about = "Parses a curl command")]
+    Parse {
+        /// The input curl command string
+        command: String,
+
+        /// Specifies which part of the curl command to parse (method, header, data, flag, url)
+        #[arg(short = 'p', long = "part", value_name = "PART")]
+        part: Option<CurlCommand>,
+    },
+}
+
 fn main() {
-    let matches = Command::new("nomcurl")
-        .version("0.1.0")
-        .about("A CLI tool to parse and manipulate curl commands")
-        .subcommand_required(true)
-        .arg_required_else_help(true)
-        .subcommand(
-            Command::new("parse")
-                .about("Parses a curl command")
-                .arg(
-                    Arg::new("command")
-                        .help("The input curl command string")
-                        .required(true)
-                        .index(1),
-                )
-                .arg(
-                    Arg::new("part")
-                        .short('p')
-                        .long("part")
-                        .value_name("PART")
-                        .help("Specifies which part of the curl command to parse (method, header, data, flag, url)")
-                        .required(false)
-                        .value_parser(clap::value_parser!(CurlCommand)),
-                ),
-        )
-        .get_matches();
+    let cli = Cli::parse();
 
-    match matches.subcommand() {
-        Some(("parse", sub_matches)) => {
-            let command = sub_matches.get_one::<String>("command").unwrap();
-            let part = sub_matches.get_one::<CurlCommand>("part");
-
-            match curl_cmd_parse(command) {
-                Ok((_remaining, curls)) => {
-                    if let Some(part) = part {
-                        match part {
-                            CurlCommand::Method => {
-                                for curl in curls.iter().filter(|c| matches!(c, Curl::Method(_))) {
-                                    println!("{:?}", curl);
-                                }
-                            }
-                            CurlCommand::Header => {
-                                for curl in curls.iter().filter(|c| matches!(c, Curl::Header(_))) {
-                                    println!("{:?}", curl);
-                                }
-                            }
-                            CurlCommand::Data => {
-                                for curl in curls.iter().filter(|c| matches!(c, Curl::Data(_))) {
-                                    println!("{:?}", curl);
-                                }
-                            }
-                            CurlCommand::Flag => {
-                                for curl in curls.iter().filter(|c| matches!(c, Curl::Flag(_))) {
-                                    println!("{:?}", curl);
-                                }
-                            }
-                            CurlCommand::Url => {
-                                for curl in curls.iter().filter(|c| matches!(c, Curl::URL(_))) {
-                                    println!("{:?}", curl);
-                                }
+    match cli.command {
+        Commands::Parse { command, part } => match curl_cmd_parse(&command) {
+            Ok((_remaining, curls)) => {
+                if let Some(part) = part {
+                    match part {
+                        CurlCommand::Method => {
+                            for curl in curls.iter().filter(|c| matches!(c, Curl::Method(_))) {
+                                println!("{:?}", curl);
                             }
                         }
-                    } else {
-                        for curl in curls {
-                            println!("{:?}", curl);
+                        CurlCommand::Header => {
+                            for curl in curls.iter().filter(|c| matches!(c, Curl::Header(_))) {
+                                println!("{:?}", curl);
+                            }
+                        }
+                        CurlCommand::Data => {
+                            for curl in curls.iter().filter(|c| matches!(c, Curl::Data(_))) {
+                                println!("{:?}", curl);
+                            }
+                        }
+                        CurlCommand::Flag => {
+                            for curl in curls.iter().filter(|c| matches!(c, Curl::Flag(_))) {
+                                println!("{:?}", curl);
+                            }
+                        }
+                        CurlCommand::Url => {
+                            for curl in curls.iter().filter(|c| matches!(c, Curl::URL(_))) {
+                                println!("{:?}", curl);
+                            }
                         }
                     }
+                } else {
+                    for curl in curls {
+                        println!("{:?}", curl);
+                    }
                 }
-                Err(e) => eprintln!("Error parsing curl command: {:?}", e),
             }
-        }
+            Err(e) => eprintln!("Error parsing curl command: {:?}", e),
+        },
         _ => {
             Command::new("nomcurl").print_help().unwrap();
             println!();
