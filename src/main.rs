42ce0a80b15f70e575dd0f6d2015164895655948
ohmycloud <ohmycloudy@gmail.com>
2024-12-29 23:28:58 +0800

    reduce code duplication

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,19 @@ pub enum CurlCommand {
     Url,
 }
 
+impl CurlCommand {
+    fn matches_curl(&self, curl: &Curl) -> bool {
+        match (self, curl) {
+            (CurlCommand::Method, Curl::Method(_)) => true,
+            (CurlCommand::Header, Curl::Header(_)) => true,
+            (CurlCommand::Data, Curl::Data(_)) => true,
+            (CurlCommand::Flag, Curl::Flag(_)) => true,
+            (CurlCommand::Url, Curl::URL(_)) => true,
+            _ => false,
+        }
+    }
+}
+
 #[derive(Parser)]
 #[command(name = "nomcurl")]
 #[command(version = "0.1.0")]
@@ -41,38 +54,11 @@ fn main() {
     match cli.command {
         Commands::Parse { command, part } => match curl_cmd_parse(&command) {
             Ok((_remaining, curls)) => {
-                if let Some(part) = part {
-                    match part {
-                        CurlCommand::Method => {
-                            for curl in curls.iter().filter(|c| matches!(c, Curl::Method(_))) {
-                                println!("{:?}", curl);
-                            }
-                        }
-                        CurlCommand::Header => {
-                            for curl in curls.iter().filter(|c| matches!(c, Curl::Header(_))) {
-                                println!("{:?}", curl);
-                            }
-                        }
-                        CurlCommand::Data => {
-                            for curl in curls.iter().filter(|c| matches!(c, Curl::Data(_))) {
-                                println!("{:?}", curl);
-                            }
-                        }
-                        CurlCommand::Flag => {
-                            for curl in curls.iter().filter(|c| matches!(c, Curl::Flag(_))) {
-                                println!("{:?}", curl);
-                            }
-                        }
-                        CurlCommand::Url => {
-                            for curl in curls.iter().filter(|c| matches!(c, Curl::URL(_))) {
-                                println!("{:?}", curl);
-                            }
-                        }
-                    }
-                } else {
-                    for curl in curls {
-                        println!("{:?}", curl);
-                    }
+                let filtered_curls = curls
+                    .iter()
+                    .filter(|c| part.map_or(true, |part_type| part_type.matches_curl(c)));
+                for curl in filtered_curls {
+                    println!("{:?}", curl);
                 }
             }
             Err(e) => eprintln!("Error parsing curl command: {:?}", e),
