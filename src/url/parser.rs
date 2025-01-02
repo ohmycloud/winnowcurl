371a88b2cc78ce19f565dd6440f22e81957abd4b
ohmycloud <ohmycloudy@gmail.com>
2025-01-02 16:09:38 +0800

    remove error value from return

--- src/url/parser.rs
+++ src/url/parser.rs
@@ -1,11 +1,10 @@
+use super::protocol::Schema;
 use clap::builder::TypedValueParser;
 use winnow::ascii::{alpha1, till_line_ending};
 use winnow::combinator::{opt, preceded, seq};
 use winnow::combinator::{separated, separated_pair};
-use winnow::error::InputError;
 use winnow::token::take_until;
 use winnow::{PResult, Parser};
-use super::protocol::Schema;
 
 #[derive(Debug, Clone)]
 pub struct QueryString<'a> {
@@ -29,24 +28,24 @@ pub struct CurlURL<'a> {
     fragment: Option<&'a str>,
 }
 
-fn parse_schema<'a>(s: &mut &'a str) -> PResult<Schema, InputError<&'a str>> {
+fn parse_schema<'a>(s: &mut &'a str) -> PResult<Schema> {
     let mut parser = take_until(1.., ':');
     let schema = parser.parse_next(s)?;
     let schema: Schema = schema.into();
     Ok(schema)
 }
 
-fn parse_authority<'a>(s: &mut &'a str) -> PResult<Authority<'a>, InputError<&'a str>> {
+fn parse_authority<'a>(s: &mut &'a str) -> PResult<Authority<'a>> {
     let mut parser = separated_pair(alpha1, ':', alpha1)
         .map(|(username, password)| Authority { username, password });
     preceded("://", parser).parse_next(s)
 }
 
-fn parse_auth_part<'a>(s: &mut &'a str) -> PResult<Option<Authority<'a>>, InputError<&'a str>> {
+fn parse_auth_part<'a>(s: &mut &'a str) -> PResult<Option<Authority<'a>>> {
     opt((parse_authority, "@").map(|(auth, _)| auth)).parse_next(s)
 }
 
-fn parse_domain<'a>(s: &mut &'a str) -> PResult<&'a str, InputError<&'a str>> {
+fn parse_domain<'a>(s: &mut &'a str) -> PResult<&'a str> {
     let mut parser = take_until(1.., '/');
     if s.starts_with("://") {
         preceded("://", parser).parse_next(s)
@@ -55,7 +54,7 @@ fn parse_domain<'a>(s: &mut &'a str) -> PResult<&'a str, InputError<&'a str>> {
     }
 }
 
-fn parse_uri<'a>(s: &mut &'a str) -> PResult<&'a str, InputError<&'a str>> {
+fn parse_uri<'a>(s: &mut &'a str) -> PResult<&'a str> {
     if s.contains("?") {
         let mut parser = take_until(1.., '?');
         preceded('/', parser).parse_next(s)
@@ -64,34 +63,32 @@ fn parse_uri<'a>(s: &mut &'a str) -> PResult<&'a str, InputError<&'a str>> {
     }
 }
 
-fn parse_params<'a>(s: &mut &'a str) -> PResult<QueryString<'a>, InputError<&'a str>> {
+fn parse_params<'a>(s: &mut &'a str) -> PResult<QueryString<'a>> {
     separated_pair(take_until(1.., "="), '=', take_until(1.., "&"))
         .map(|(key, value)| QueryString { key, value })
         .parse_next(s)
 }
 
-fn parse_query_string<'a>(s: &mut &'a str) -> PResult<Vec<QueryString<'a>>, InputError<&'a str>> {
+fn parse_query_string<'a>(s: &mut &'a str) -> PResult<Vec<QueryString<'a>>> {
     separated(1.., parse_params, "&").parse_next(s)
 }
 
-fn parse_query_part<'a>(s: &mut &'a str) -> PResult<Option<Vec<QueryString<'a>>>, InputError<&'a str>> {
+fn parse_query_part<'a>(s: &mut &'a str) -> PResult<Option<Vec<QueryString<'a>>>> {
     opt(("?", parse_query_string).map(|(_, queries)| queries)).parse_next(s)
 }
 
-fn parse_fragment<'a>(s: &mut &'a str) -> PResult<Option<&'a str>, InputError<&'a str>> {
+fn parse_fragment<'a>(s: &mut &'a str) -> PResult<Option<&'a str>> {
     opt(('#', till_line_ending).map(|(_, fragment)| fragment)).parse_next(s)
 }
 
-pub fn parse_url<'a>(s: &mut &'a str) -> PResult<CurlURL<'a>, InputError<&'a str>> {
-    seq!(
-        CurlURL {
-            schema: parse_schema,
-            authority: parse_auth_part,
-            path: parse_domain,
-            uri: parse_uri,
-            queries: parse_query_part,
-            fragment: parse_fragment,
-        }
-    )
+pub fn parse_url<'a>(s: &mut &'a str) -> PResult<CurlURL<'a>> {
+    seq!(CurlURL {
+        schema: parse_schema,
+        authority: parse_auth_part,
+        path: parse_domain,
+        uri: parse_uri,
+        queries: parse_query_part,
+        fragment: parse_fragment,
+    })
     .parse_next(s)
 }
