5635ebfd66d9283f3ba2c7ec981a80de1e2ab79f
ohmycloud <ohmycloudy@gmail.com>
2025-01-02 16:40:39 +0800

    use rstest instead of rust's own test

--- src/url/parser.rs
+++ src/url/parser.rs
@@ -4,7 +4,9 @@ use winnow::ascii::{alpha1, till_line_ending};
 use winnow::combinator::{opt, preceded, seq};
 use winnow::combinator::{separated, separated_pair};
 use winnow::token::take_until;
-use winnow::{PResult, Parser};
+use winnow::{Located, PResult, Parser};
+
+type Input<'a> = Located<&'a str>;
 
 #[derive(Debug, Clone)]
 pub struct QueryString<'a> {
@@ -28,24 +30,24 @@ pub struct CurlURL<'a> {
     fragment: Option<&'a str>,
 }
 
-fn parse_schema<'a>(s: &mut &'a str) -> PResult<Schema> {
+fn parse_schema<'a>(s: &mut Input<'a>) -> PResult<Schema> {
     let mut parser = take_until(1.., ':');
     let schema = parser.parse_next(s)?;
     let schema: Schema = schema.into();
     Ok(schema)
 }
 
-fn parse_authority<'a>(s: &mut &'a str) -> PResult<Authority<'a>> {
+fn parse_authority<'a>(s: &mut Input<'a>) -> PResult<Authority<'a>> {
     let mut parser = separated_pair(alpha1, ':', alpha1)
         .map(|(username, password)| Authority { username, password });
     preceded("://", parser).parse_next(s)
 }
 
-fn parse_auth_part<'a>(s: &mut &'a str) -> PResult<Option<Authority<'a>>> {
+fn parse_auth_part<'a>(s: &mut Input<'a>) -> PResult<Option<Authority<'a>>> {
     opt((parse_authority, "@").map(|(auth, _)| auth)).parse_next(s)
 }
 
-fn parse_domain<'a>(s: &mut &'a str) -> PResult<&'a str> {
+fn parse_domain<'a>(s: &mut Input<'a>) -> PResult<&'a str> {
     let mut parser = take_until(1.., '/');
     if s.starts_with("://") {
         preceded("://", parser).parse_next(s)
@@ -54,7 +56,7 @@ fn parse_domain<'a>(s: &mut &'a str) -> PResult<&'a str> {
     }
 }
 
-fn parse_uri<'a>(s: &mut &'a str) -> PResult<&'a str> {
+fn parse_uri<'a>(s: &mut Input<'a>) -> PResult<&'a str> {
     if s.contains("?") {
         let mut parser = take_until(1.., '?');
         preceded('/', parser).parse_next(s)
@@ -63,25 +65,25 @@ fn parse_uri<'a>(s: &mut &'a str) -> PResult<&'a str> {
     }
 }
 
-fn parse_params<'a>(s: &mut &'a str) -> PResult<QueryString<'a>> {
+fn parse_params<'a>(s: &mut Input<'a>) -> PResult<QueryString<'a>> {
     separated_pair(take_until(1.., "="), '=', take_until(1.., "&"))
         .map(|(key, value)| QueryString { key, value })
         .parse_next(s)
 }
 
-fn parse_query_string<'a>(s: &mut &'a str) -> PResult<Vec<QueryString<'a>>> {
+fn parse_query_string<'a>(s: &mut Input<'a>) -> PResult<Vec<QueryString<'a>>> {
     separated(1.., parse_params, "&").parse_next(s)
 }
 
-fn parse_query_part<'a>(s: &mut &'a str) -> PResult<Option<Vec<QueryString<'a>>>> {
+fn parse_query_part<'a>(s: &mut Input<'a>) -> PResult<Option<Vec<QueryString<'a>>>> {
     opt(("?", parse_query_string).map(|(_, queries)| queries)).parse_next(s)
 }
 
-fn parse_fragment<'a>(s: &mut &'a str) -> PResult<Option<&'a str>> {
+fn parse_fragment<'a>(s: &mut Input<'a>) -> PResult<Option<&'a str>> {
     opt(('#', till_line_ending).map(|(_, fragment)| fragment)).parse_next(s)
 }
 
-pub fn parse_url<'a>(s: &mut &'a str) -> PResult<CurlURL<'a>> {
+pub fn parse_url<'a>(s: &mut Input<'a>) -> PResult<CurlURL<'a>> {
     seq!(CurlURL {
         schema: parse_schema,
         authority: parse_auth_part,
@@ -91,4 +93,18 @@ pub fn parse_url<'a>(s: &mut &'a str) -> PResult<CurlURL<'a>> {
         fragment: parse_fragment,
     })
     .parse_next(s)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rstest::*;
+
+    #[rstest]
+    #[case("https://", Schema::HTTPS)]
+    fn test_parse_schema(#[case] input: String, #[case] expected: Schema) {
+        let mut input = Located::new(input.as_str());
+        let schema = parse_schema(&mut input).unwrap();
+        assert_eq!(schema, expected)
+    }
 }
--- tests/url_parser_test.rs
+++ tests/url_parser_test.rs
@@ -1,8 +1,10 @@
 use nomcurl::url::parser::parse_url;
+use winnow::Located;
 
 #[test]
 fn test_parse_with_auth() {
-    let mut input = "https://user:passwd@github.com/rust-lang/rust/issues?labels=E-easy&state=open#ABC";
+    let input = "https://user:passwd@github.com/rust-lang/rust/issues?labels=E-easy&state=open#ABC";
+    let mut input = Located::new(input);
     let url = parse_url(&mut input);
 
     if let Ok(url) = url {
@@ -13,6 +15,7 @@ fn test_parse_with_auth() {
 #[test]
 fn test_parse_without_auth() {
     let mut input = "https://github.com/rust-lang/rust/issues";
+    let mut input = Located::new(input);
     let url = parse_url(&mut input);
 
     if let Ok(url) = url {
